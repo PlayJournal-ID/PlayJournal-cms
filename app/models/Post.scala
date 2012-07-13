866212bb675da3d74a850adc642384d53311ec2b
Alex Xandra Albert Sim <bertzzie@gmail.com>
2012-07-13 12:02:04 +0700

    Simple post form created.

---
+++ app/models/Post.scala
@@ -0,0 +1,37 @@
+package models
+
+import play.api.db._
+import anorm._
+import anorm.SqlParser._
+import java.util.Date
+import play.api.Play.current
+
+case class Post(id: Pk[Long], title: String, content: String, created: Date, lastUpdate: Date, writer: Long)
+
+object Post {
+    val simple = {
+        get[Pk[Long]]("post.id") ~
+            get[String]("post.title") ~
+            get[String]("post.content") ~
+            get[Date]("post.created") ~
+            get[Date]("post.lastUpdate") ~
+            get[Long]("post.writer") map {
+                case id ~ title ~ content ~ created ~ lastUpdate ~ writer => Post(id, title, content, created, lastUpdate, writer)
+            }
+    }
+    
+    def create(title: String, content: String, userId: Long) = {
+        DB.withConnection { implicit connection =>
+            SQL("""
+                   INSERT INTO post (title, content, created, writer) 
+                   VALUES ({title}, {content}, NOW(), {writer})
+                """)
+                   .on(
+                       'title -> title,
+                       'content -> content,
+                       'writer -> userId
+                   )
+                   .executeInsert()
+        }
+    }
+}
